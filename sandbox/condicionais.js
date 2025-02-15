2c3d0e9920c877dfb0da4a1d72f3a6ae1e26df21
FIC <fic@info.local>
2025-02-14 22:01:54 -0300

    exercicio 2

--- sandbox/condicionais.js
+++ sandbox/condicionais.js
@@ -104,6 +104,158 @@ switch (numx3) {
 }
 
 
+// versa0 2 
+
+let num1 = parseFloat(prompt("Insira o primeiro número:"));
+let num2 = parseFloat(prompt("Insira outro número:"));
+let operacao = prompt("Insira a operação. Ex: +, -, *, /.");
+
+let resultado = null;
+
+switch (operacao) {
+  case "+":
+
+    resultado = num1 + num2;
+
+    break;
+
+  case "-":
+
+    resultado = num1 - num2;
+
+    break;
+
+  case "*":
+
+    resultado = num1 * num2;
+
+    break;
+
+  case "/":
+
+    if (num2 == 0) {
+        console.log("Você não pode dividir um número por zero.");
+    } else {
+        resultado = num1 / num2;
+        console.log("resultado", resultado);
+    }
+
+    break;
+
+  default:
+
+    console.log("Operador inválido.");
+
+    break;
+}
+
+if (operacao != "/") {
+
+    console.log("resultado", resultado);
+
+}
+
+
+
 // Exercicio 2
 
-let qualmaior1 = prompt("Digite um Numero")
+let qualmaior1 = prompt("Digite um Numero");
+let qualmaior2 = prompt("Digete o segundo numero");
+let qualmaior3 = prompt("Digite o terceiro numero");
+
+console.log("Numeros digitados: " + qualmaior1 + ", " + qualmaior2 + ", " + qualmaior3)
+
+if(qualmaior1 > qualmaior2 && qualmaior1 > qualmaior3){
+
+    console.log("O numero maior é: " + qualmaior1);
+}
+else if(qualmaior2 > qualmaior1 && qualmaior2 > qualmaior3){
+    console.log("Onumero maior é:" + qualmaior2)
+}
+else if(qualmaior3 > qualmaior1 && qualmaior3 > qualmaior2){
+    console.log("O numero maior e: "+ qualmaior3) 
+}
+
+
+// Exercicio 3
+
+
+let usuario = prompt("Digite o usuario");
+let senha = prompt("Digite a sua senha");
+
+
+if(usuario == "admin"){
+    if(senha == "1234"){
+        console.log("Acesso liberado")
+        alert("Acesso liberado");
+    } 
+    else {
+        console.log("Acesso Negado")
+        alert("Acesso Negado");
+       
+    }
+      
+}
+
+//Nivel Avancado
+
+// exercicio 1
+
+        //triangulo
+
+
+//Exercio 2
+
+
+let notax = prompt("Insira uma nota (0-100)");
+
+if (notax >= 90 && notax <= 100) {
+    console.log("Conceito A " + notax);
+    alert("Conceito A " + notax);
+} 
+else if (notax >= 80 && notax <= 89) {
+    console.log("Conceito B " + notax);
+    alert("Conceito B " + notax);
+}
+ else if (notax >= 70 && notax <= 79) {
+    console.log("Conceito C " + notax);
+    alert("Conceito C " + notax);
+} 
+else if (notax >= 60 && notax <= 69) {
+    console.log("Conceito D " + notax);
+    alert("Conceito D " + notax);
+} 
+else {
+    console.log("Conceito F " + notax);
+    alert("Conceito F " + notax);
+}
+
+
+
+// Calculo de IMC
+
+let peso = prompt("Entre com seu peso")
+let altura = prompt("Entre com a sua altura")
+
+
+// Ano Bissexto - exercico 4 
+
+let ano = parseint(Insira o ano){
+
+if(ano % 4 == 0)
+    console.log(E bissexto);
+}
+
+else{
+
+if(ano % 400 == 0)
+    console.log("E bissexto");
+
+}
+
+else{
+console.log("Nao e bissexto");
+
+
+}
+
